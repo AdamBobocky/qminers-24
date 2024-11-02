6fa53aebfada4d00ffd5cfe0e45d15844b112120
Adam Bobocky <adambobocky@gmail.com>
2024-11-02 11:27:21 +0100

    Fix wording on charts in research/7_team_strength_stationarity.

--- research/7_team_strength_stationarity/charting.py
+++ research/7_team_strength_stationarity/charting.py
@@ -25,31 +25,24 @@ avg_odds = pd.concat([home_avg_odds, away_avg_odds])
 # Group by Season and TeamID to get the final average odds per season for each team
 final_avg_odds = avg_odds.groupby(['Season', 'TeamID'])['Avg_Odds'].mean().reset_index()
 
-# Display the result
-for season in final_avg_odds['Season'].unique():
-    print(f"Season: {season}")
-    season_data = final_avg_odds[final_avg_odds['Season'] == season]
-    print(season_data[['TeamID', 'Avg_Odds']].to_string(index=False))
-    print("\n")
-
 pivot_df = final_avg_odds.pivot(index='Season', columns='TeamID', values='Avg_Odds')
 
 fig = go.Figure()
 
 for team in pivot_df.columns:
     fig.add_trace(go.Scatter(
-        x=pivot_df.index, 
+        x=pivot_df.index,
         y=pivot_df[team],
         mode='lines+markers',
         name=team
     ))
 
 fig.update_layout(
-    title="Teams' Win Rates Over Seasons",
-    xaxis_title="Season",
-    yaxis_title="Win Rate",
+    title='Teams average odds after inverse sigmoid',
+    xaxis_title='Season',
+    yaxis_title='Win Rate',
     xaxis=dict(tickvals=sorted(df['Season'].unique())),
-    template="plotly_white"
+    template='plotly_white'
 )
 
 fig.show()
